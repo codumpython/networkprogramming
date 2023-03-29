480b46dd07f7a161eca270af3186a3131c87e3d5
İbrahim EDİZ <pythonegitimiediz@gmail.com>
2023-03-29 12:19:28 +0000

    29032023_2

--- Documents/createfile.py
+++ Documents/createfile.py
@@ -2,6 +2,7 @@ import os
 liste = os.listdir("Exercises/")
 fileName = "first"
 for item in liste:
-    with open(f"Exercises/{item}/{fileName}","a+") as fileContent:
+    with open(f"Exercises/{item}/{fileName}.py","a+") as fileContent:
         fileContent.write("""""")
+    
 
