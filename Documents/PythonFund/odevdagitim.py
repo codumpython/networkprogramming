39fdefd43b159d477f6aa018692f1c1236fbb7a0
İbrahim EDİZ <pythonegitimiediz@gmail.com>
2023-04-08 16:12:43 +0000

    08042023

---
+++ Documents/PythonFund/odevdagitim.py
@@ -0,0 +1,45 @@
+liste = ["Adem","Ali","Alim","Alişan","Berkay","Buse","Ceren","Deniz","Esra","Furkan","Hülya","Mehmet","Mehmet Can","Melisa","Özge","Sadık","Yelda"]
+konuListe  = ["DHCPv6 yapılandırması ",
+"IPv4 ten IPv6 geçiş süreçleri",
+"SDN Nedir, Bir SND in kurulumu ve Yapılandırılması",
+"subnetting",
+"Kablosuz Ağlar",
+"802.x Standarları",
+"Kablosuz Ağ Güvenliği",
+"TCP/IP Modeli (Detaylı) ",
+"Ağ Güvenliği için SNMP kullanımı",
+"SaaS",
+"Switching,Routing",
+"Network Erişim Kontrolü",
+"Ağ Protokolleri",
+"LDAP kurulumu ve yapılandırılması","Konu2","Konu3","Konu4","Konu5"]
+import random as rnd
+rnd.shuffle(konuListe)
+rnd.shuffle(liste)
+print(*zip(liste,konuListe),sep="\n")
+
+
+#--------------------------
+('Sadık', 'DHCPv6 yapılandırması ')
+('Alişan', 'Network Design with GPT4,GPT3.5,GPT3')
+('Ceren', 'TCP/IP Modeli (Detaylı) ')
+('Mehmet Can', 'MacStealer: Wi-Fi Client Isolation Bypass')
+('Adem', 'Ağ Protokolleri')
+('Ali', 'Network Erişim Kontrolü')
+('Esra', 'SDN Nedir, Bir SND in kurulumu ve Yapılandırılması')
+('Alim', '802.x Standarları')
+('Mehmet', 'Switching,Routing')
+('Buse', 'Kablosuz Ağlar')
+('Melisa', 'IPv4 ten IPv6 geçiş süreçleri')
+('Furkan', 'Network Configuration in Cloud Computing')
+('Deniz', 'Kablosuz Ağ Güvenliği')
+('Hülya', 'LDAP kurulumu ve yapılandırılması')
+('Özge', 'Ağ Güvenliği için SNMP kullanımı')
+('Berkay', 'ARP')
+('Yelda', 'SaaS')
+('Tuğçe', 'VLAN Nedir(Detaylı),Kurulum ve Yapılandırılması')
+('Mehmet Kaan', 'Statik ve dinamik yönlendirme')
+('Durmuş', 'Ağ Protokolleri(Detaylı)')
+('Selin', 'Network Erişim Kontrolü')
+('Yasemin','Ağ Topolojileri')
+#----------------------------
---
+++ konular.md
@@ -0,0 +1,13 @@
+DHCPv6 yapılandırması 
+IPv4 ten IPv6 geçiş süreçleri
+SDN Nedir, Bir SND in kurulumu ve Yapılandırılması
+subnetting
+Kablosuz Ağlar
+802.x Standarları
+Kablosuz Ağ Güvenliği
+TCP/IP Modeli (Detaylı) 
+Ağ Güvenliği için SNMP kullanımı
+SaaS
+Switching,Routing
+Network Erişim Kontrolü
+Ağ Protokolleri
